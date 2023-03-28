61ec715ab040d581fb769a67c353ea38fe926606
JaviGalFer <fgalfer299@g.educaand.es>
2023-03-28 13:26:31 +0200

    CorreciónAnimales

--- 2Trimestre/C9.3/Mascotas/Inventario.java
+++ 2Trimestre/C9.3/Mascotas/Inventario.java
@@ -1,229 +1,50 @@
-import java.util.NoSuchElementException;
 import java.util.ArrayList;
-import java.util.Scanner;
-/**
- * @author Fco Javier Gallego Fernández
- */
+
 public class Inventario {
-    private ArrayList<Mascota> inventario; // ArrayList para almacenar las mascotas del inventario
+    private ArrayList<Mascota> animales;
 
     public Inventario() {
-        inventario = new ArrayList<>();
+        animales = new ArrayList<Mascota>();
     }
 
-    // Método para mostrar la lista de animales (solo tipo y nombre, 1 línea por animal)
     public void mostrarListaAnimales() {
-        System.out.println("Lista de animales en el inventario:");
-        for (Mascota mascota : inventario) {
+        for (Mascota mascota : animales) {
             System.out.println(mascota.getClass().getSimpleName() + ": " + mascota.getNombre());
         }
-        System.out.println();
     }
 
-    // Método para mostrar todos los datos de un animal concreto
     public void mostrarDatosAnimal(String nombre) {
-        for (Mascota mascota : inventario) {
+        for (Mascota mascota : animales) {
             if (mascota.getNombre().equals(nombre)) {
-                System.out.println(mascota.toString());
+                mascota.muestra();
                 return;
             }
         }
-        System.out.println("No se encontró un animal con el nombre " + nombre + " en el inventario.");
+        System.out.println("No se encontró el animal " + nombre);
     }
 
-    // Método para mostrar todos los datos de todos los animales
     public void mostrarDatosAnimales() {
-        System.out.println("Datos de todos los animales en el inventario:");
-        for (Mascota mascota : inventario) {
-            System.out.println(mascota.toString());
+        for (Mascota mascota : animales) {
+            mascota.muestra();
+            System.out.println();
         }
-        System.out.println();
     }
 
-    // Método para insertar animales en el inventario
-    public void insertarAnimal() {
-        Scanner sc = new Scanner(System.in);
-    
-        System.out.println("¿Qué tipo de animal quieres insertar?");
-        System.out.println("1. Perro");
-        System.out.println("2. Gato");
-        System.out.println("3. Loro");
-        System.out.println("4. Canario");
-    
-        int opcion = sc.nextInt();
-        sc.nextLine();
-    
-        switch (opcion) {
-            case 1://///////     PERRO     ///////////////////
-                System.out.println("Introduce el nombre del perro: ");
-                String nombre = System.console().readLine();
-
-                System.out.println("Introduce la edad del perro: ");
-                /*int edad = sc.nextInt();
-                sc.nextLine();*/
-                int edad = Integer.parseInt(System.console().readLine());
-
-                System.out.println("Introduce la raza del perro: ");
-                String raza = System.console().readLine();
-    
-                System.out.println("Introduce el estado del animal (vivo = true, muerto = false):");
-                boolean estado = Boolean.parseBoolean(System.console().readLine());
-                //sc.nextLine();
-                
-                System.out.println("Introduce la fecha de nacimiento: ");
-                String fechaNacimiento = System.console().readLine();
-    
-                System.out.println("¿Tiene pulgas? (true o false): ");
-                /*boolean pulgas = sc.nextBoolean();
-                sc.nextLine();*/
-                boolean pulgas = Boolean.parseBoolean(System.console().readLine());
-    
-                Perro perro = new Perro(nombre, edad, estado, fechaNacimiento, raza, pulgas);
-                inventario.add(perro);
-                System.out.println("Perro insertado correctamente.");
-                break;
-    
-            case 2://///////////////// GATO ///////////////////
-
-                System.out.println("Introduce el nombre del gato: ");
-                nombre = sc.next();
-
-                System.out.println("Introduce la edad del gato: ");
-                edad = sc.nextInt();
-                sc.nextLine();
-
-                System.out.println("Introduce el estado del animal (vivo = true, muerto = false):");
-                estado = sc.nextBoolean();
-                sc.nextLine();
-                
-                System.out.println("Introduce la fecha de nacimiento: ");
-                fechaNacimiento = sc.next();
-                sc.nextLine();
-
-                System.out.println("Introduce el color del gato:");
-                String color = sc.nextLine();
-
-                boolean peloLargo = false;
-                /**
-                System.out.println("¿Tiene el pelo largo? (true o false):");
-                boolean peloLargo = sc.nextBoolean();
-                sc.nextLine();
-                
-
-                boolean peloLargo = false;
-                while (true) {
-                    try {
-                        System.out.println("¿Tiene el pelo largo? (true o false):");
-                        peloLargo = sc.nextBoolean();
-                        sc.nextLine(); // limpiar buffer
-                        break;
-                    } catch (NoSuchElementException e) {
-                        System.out.println("Entrada inválida, por favor ingrese un valor válido.");
-                        sc.nextLine(); // descarta la entrada inválida
-                    }
-                }
-                */
-                Gato gato = new Gato(nombre, edad, estado, fechaNacimiento, color, peloLargo);
-                inventario.add(gato);
-                System.out.println("Gato insertado correctamente.");
-                break;
-    
-            case 3:///////////  LORO  /////////////////
-                System.out.println("Introduce el nombre del loro: ");
-                nombre = sc.next();
-
-                System.out.println("Introduce la edad del loro: ");
-                edad = sc.nextInt();
-                sc.nextLine();
-
-                System.out.println("Introduce el pico del loro: ");
-                double pico = sc.nextDouble();
-                sc.nextLine();
-
-                System.out.println("¿El loro puede volar? (Puede = true, no puede = false)");
-                boolean vuela = sc.nextBoolean();
-                sc.nextLine();
-
-                System.out.println("Introduce el estado del animal (vivo = true, muerto = false):");
-                estado = sc.nextBoolean();
-                sc.nextLine();
-                
-                System.out.println("Introduce la fecha de nacimiento: ");
-                fechaNacimiento = sc.next();
-                sc.nextLine();
-
-                System.out.println("Introduce el origen del loro:");
-                String origen = sc.nextLine();
-    
-                System.out.println("¿Habla el loro? (true o false):");
-                boolean habla = sc.nextBoolean();
-                sc.nextLine();
-    
-                Loro loro = new Loro(nombre, edad, estado, fechaNacimiento, pico, vuela, origen, habla);
-                inventario.add(loro);
-                System.out.println("Loro insertado correctamente.");
-                break;
-    
-            case 4:///////////////////  CANARIO   ///////////////////
-                
-                System.out.println("Introduce el nombre del canario: ");
-                nombre = sc.next();
-                
-
-                System.out.println("Introduce la edad del canario: ");
-                edad = sc.nextInt();
-                sc.nextLine();
-
-                System.out.println("Introduce el estado del animal (vivo = true, muerto = false):");
-                estado = sc.nextBoolean();
-                sc.nextLine();
-                
-                System.out.println("Introduce la fecha de nacimiento: ");
-                fechaNacimiento = sc.next();
-
-                System.out.println("Introduce el pico del loro: ");
-                pico = sc.nextDouble();
-
-                System.out.println("¿El canario puede volar? (Puede volar = true, no puede volar = false)");
-                vuela = sc.nextBoolean();
-                sc.nextLine();
-
-                System.out.println("Introduce el color del canario:");
-                String colorCanario = sc.nextLine();
-                sc.nextLine();
-    
-                System.out.println("¿Canta el canario? (true o false):");
-                boolean canta = sc.nextBoolean();
-                sc.nextLine();
-    
-                Canario canario = new Canario(nombre, edad, estado, fechaNacimiento, pico, vuela, colorCanario, canta);
-                inventario.add(canario);
-                System.out.println("Canario insertado correctamente.");
-                break;
-    
-            default:
-                System.out.println("Opción incorrecta. No se ha insertado ningún animal.");
-                break;
-        }
-        sc.close();
+    public void insertarAnimal(Mascota animal) {
+        animales.add(animal);
     }
-    
 
-    // Método para eliminar animales del inventario
     public void eliminarAnimal(String nombre) {
-        for (int i = 0; i < inventario.size(); i++) {
-            if (inventario.get(i).getNombre().equals(nombre)) {
-                inventario.remove(i);
-                System.out.println("Se eliminó un animal con el nombre " + nombre + " del inventario.");
+        for (Mascota mascota : animales) {
+            if (mascota.getNombre().equals(nombre)) {
+                animales.remove(mascota);
                 return;
             }
         }
-        System.out.println("No se encontró un animal con el nombre " + nombre + " en el inventario.");
+        System.out.println("No se encontró el animal " + nombre);
     }
 
-    // Método para vaciar el inventario
     public void vaciarInventario() {
-        inventario.clear();
-        System.out.println("Se vació el inventario.");
+        animales.clear();
     }
 }
--- 2Trimestre/C9.3/Mascotas/PruebaInvent.java
+++ 2Trimestre/C9.3/Mascotas/PruebaInvent.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 /*
  * @author Fco Javier Gallego Fernández
  */
 public class PruebaInvent {
-
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner s = new Scanner(System.in);
     static Inventario inventario = new Inventario();
-
     public static void main(String[] args) {
-
-        int opcion = 10;
-
+        int opcion = 0;
         
         do{
-            
             System.out.println("------- MENÚ -------");
             System.out.println("1. Mostrar lista de animales");
             System.out.println("2. Mostrar datos de un animal");
@@ -23,68 +16,107 @@ public class PruebaInvent {
             System.out.println("4. Insertar un animal");
             System.out.println("5. Eliminar un animal");
             System.out.println("6. Vaciar el inventario");
-            System.out.println("0. Salir");
+            System.out.println("7. Salir");
+            System.out.print("Selecciona una opción: ");
+            try {//Comprobamos que el valor sea correcto
+                opcion=Integer.parseInt(s.next());
 
-            /**
-            opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumimos el salto de línea
+            }catch(NumberFormatException e){// si el valor introducido no coincide con int, se muestra un mensaje de error
             
-            try {
-                opcion = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("La opción ingresada no es válida. Por favor ingrese una opción válida.");
-                scanner.next(); // Limpia el buffer de entrada
+                System.out.println("\nOpción no válida. Elija del 1 al 7.");
             }
-            */
 
-            try {
-                /**
-                opcion = scanner.nextInt();
-                scanner.nextLine();
-                
-                while (!scanner.hasNextLine()); // Espera hasta que haya una entrada válida
-                */
-                
-                opcion = Integer.parseInt(scanner.nextLine());
+            //Menú principal
             switch (opcion) {
-                case 1:
+                case 1://Listado de animales
+                    System.out.println("Listado de animales:");
                     inventario.mostrarListaAnimales();
                     break;
-                case 2:
+                case 2://Mostrar datos de un animal
                     System.out.println("Introduce el nombre del animal:");
-                    String nombre = scanner.nextLine();
+                    String nombre = s.nextLine();
                     inventario.mostrarDatosAnimal(nombre);
                     break;
-                case 3:
+                case 3://Mostrar datos de animales
                     inventario.mostrarDatosAnimales();
                     break;
-                case 4:
-                    inventario.insertarAnimal();
+                case 4://Insertar animal
+                    System.out.println("Seleccione el tipo de animal a agregar:");
+                    System.out.println("1. Perro");
+                    System.out.println("2. Gato");
+                    System.out.println("3. Canario");
+                    System.out.println("4. Loro");
+                    int tipo = Integer.parseInt(System.console().readLine());
+                    
+                    System.out.print("Ingrese el nombre del animal: ");
+                    String nombreNuevo = System.console().readLine();
+                    
+                    System.out.print("Ingrese la edad del animal: ");
+                    int edadNuevo = Integer.parseInt(System.console().readLine());
+                    
+                    System.out.print("Ingrese el estado del animal (true/false): ");
+                    boolean estadoNuevo = Boolean.parseBoolean(System.console().readLine());
+                    
+                    System.out.print("Ingrese la fecha de nacimiento del animal: ");
+                    String fechaNuevo = System.console().readLine();
+                    //Tipo de animal
+                    switch (tipo) {
+                        case 1://Perro
+                            System.out.print("Ingrese la raza del perro: ");
+                            String raza = System.console().readLine();
+                            System.out.print("¿Tiene pulgas el perro? (true/false): ");
+                            boolean pulgas = Boolean.parseBoolean(System.console().readLine());
+                            inventario.insertarAnimal(new Perro(nombreNuevo, edadNuevo, estadoNuevo, fechaNuevo, raza, pulgas));
+                            System.out.println("Perro insertado correctamente.");
+                            break;
+                            
+                        case 2://Gato
+                            System.out.print("Ingrese el color del gato: ");
+                            String color = s.next();
+                            System.out.print("¿Tiene pelo largo el gato? (true/false): ");
+                            boolean peloLargo = Boolean.parseBoolean(System.console().readLine());
+                            Gato gatoNuevo = new Gato(nombreNuevo, edadNuevo, estadoNuevo, fechaNuevo, color, peloLargo);
+                            inventario.insertarAnimal(gatoNuevo);
+                            System.out.println("Gato insertado correctamente.");
+                            break;
+                            
+                        case 3://Canario
+                            System.out.print("Ingrese el color del canario: ");
+                            String colorCanario = System.console().readLine();
+                            System.out.print("¿Canta el canario? (true/false): ");
+                            boolean canta = Boolean.parseBoolean(System.console().readLine());
+                            Canario canarioNuevo = new Canario(nombreNuevo, edadNuevo, estadoNuevo, fechaNuevo, 0.0, false, colorCanario, canta);
+                            inventario.insertarAnimal(canarioNuevo);
+                            System.out.println("Canario insertado correctamente.");
+                            break;
+                            
+                        case 4://Loro
+                            System.out.print("Ingrese el origen del loro: ");
+                            String origen = System.console().readLine();
+                            System.out.print("¿Habla?");
+                            boolean habla = Boolean.parseBoolean(System.console().readLine());
+                            Loro loroNuevo = new Loro(nombreNuevo, edadNuevo, estadoNuevo, fechaNuevo, edadNuevo, habla, origen, habla);
+                            inventario.insertarAnimal(loroNuevo);
+                            System.out.println("Loro insertado correctamente.");
+                            break;
+                        default:
+                    }
                     break;
-                case 5:
+                case 5://Eliminar animal
                     System.out.println("Introduce el nombre del animal:");
-                    nombre = scanner.nextLine();
+                    nombre = s.nextLine();
                     inventario.eliminarAnimal(nombre);
                     break;
-                case 6:
+                case 6://Vaciar inventario
                     inventario.vaciarInventario();
                     break;
-                case 0:
+                case 7://Salir del programa
                     System.out.println("Saliendo del programa...");
                     break;
-                default:
+                default://Error
                     System.out.println("Opción no válida.");
+                    break;
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Entrada inválida, por favor ingrese un valor válido.");
-            scanner.nextLine(); // descarta la entrada inválida
-            opcion = 10;
-        }catch (NoSuchElementException e) {
-            System.out.println("Entrada inválida, por favor ingrese un valor válido.");
-            scanner.nextLine(); // descarta la entrada inválida
-            opcion = 10;
-        }
-        
-        }while (opcion != 0);
+        }while(opcion!=7);//Mientras no se elija la opción 7
     }
 }
